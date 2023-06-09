b665247c63069ac5df0c51491604950cf4bb4915
joelle78 <96306220+joelle78@users.noreply.github.com>
2023-06-09 12:08:26 +0200

    leaflet

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 
 import * as path from 'path'
-import { Server } from 'socket.io'
 import { createServer } from 'http'
 import express, { response } from 'express'
-import fetch from 'node-fetch';
-
 
 const server = express()
 const http = createServer(server)
-const io = new Server(http)
 
 const port = process.env.PORT || 8000
 
-const historySize = 50
-
-let history = []
 
 // Serveer client-side bestanden
 server.use(express.static(path.resolve('public')))
@@ -27,51 +20,9 @@ server.set('views', './views')
 server.use(express.json())
 server.use(express.urlencoded({extended: true}))
 
-// Stel afhandeling van formulieren in
-server.use(express.json())
-server.use(express.urlencoded({extended: true}))
+// API QUICK START SCHIPHOL
 
-// SOCKET
-//CHATROOM
-io.on('connection', (socket) => {
-    // Log de connectie naar console
-    console.log(`user ${socket.id} connected`)
-    // Stuur de historie door, let op: luister op socket, emit op io!
-    io.emit('history', history)
 
-    // Luister naar een message van een gebruiker
-    socket.on('message', (message) => {
-        // Check de maximum lengte van de historie
-        while (history.length > historySize) {
-            history.shift()
-        }
-        // Voeg het toe aan de historie
-        history.push(message)
-        // Verstuur het bericht naar alle clients, socket id toegevoegd, en content van het bericht toegevoegd
-        io.emit('message', {uid: socket.id, message: message})
-    })
-
-    //  TEST 1 HOW MANY ACTIVE PLAYERS
-    let activePlayers = 0;
-
-    io.on('connection', (socket) => {
-        socket.on('activePlayersCount', () => {
-            let totalCount = socket.server.engine.clientsCount
-            io.emit('activePlayersCountClient', totalCount);
-        })
-
-        socket.on('disconnect', () => {
-            activePlayers--;
-            console.log(`Client disconnected. Active players: ${activePlayers}`);
-            io.emit('activePlayersCount', activePlayers);
-        });
-    });
-
-    // Luister naar een disconnect van een gebruiker
-    socket.on('disconnect', () => {
-        console.log('user disconnected')
-    })
-})
 
 // Maak een route voor de index
 server.get('/', async function (req, res) {
@@ -83,50 +34,10 @@ server.get('/', async function (req, res) {
     res.render('index', data)
 })
 
-// Route voor de stekjes page
-server.get('/stek', async function (req, res) {
+// // Route voor evenementen page
+// server.get('/evenementen', (request, response) => {
+//     response.render('evenementen')
 
-    const baseUrl = "https://api.buurtcampus-oost.fdnd.nl/api/v1"
-    const url = ('https://api.buurtcampus-oost.fdnd.nl/api/v1/stekjes')
-
-    const data = await fetch(url).then((response) => response.json())
-    res.render('stek', data)
-})
-
-// Route voor evenementen page
-server.get('/evenementen', (request, response) => {
-    response.render('evenementen')
-})
-
-// Route voor de contact page
-server.get('/contact', (request, response) => {
-    response.render('contact')
-})
-
-// Route voor de trivia begin page
-server.get('/welkom', (request, response) => {
-    response.render('welkom')
-})
-
-// Route voor de trivia vragen page
-server.get('/loading', (request, response) => {
-    response.render('loading')
-})
-
-// Route voor trivia
-server.get('/trivia', (request, response) => {
-    response.render('trivia')
-})
-
-// Route voor error
-server.get('/error', (request, response) => {
-    response.render('error')
-})
-
-// Route voor chatroom
-server.get('/chatroom', (request, response) => {
-    response.render('chatroom')
-})
 
 // De .get()-methode wordt gebruikt om een route pad te definiëren.
 server.get('/:id', (request, response) => {
@@ -140,36 +51,21 @@ server.get('/:id', (request, response) => {
     })
 })
 
-// Wanneer een client een verzoek stuurt naar het '/new'-eindpunt, zal de response.render()-functie worden opgeroepen.
-server.get('/new', (request, response) => {
-    response.render('admin')
-})
+// // Wanneer een client een verzoek stuurt naar het '/new'-eindpunt, zal de response.render()-functie worden opgeroepen.
+// server.get('/new', (request, response) => {
+//     response.render('admin')
+// })
 
-server.post('/new', (request, response) => {
-    // Het samenvoegen van de juiste API-eindpunten in de url-variabele.
-    const baseUrl = "https://api.buurtcampus-oost.fdnd.nl/api/v1"
-    const url = baseUrl + '/stekjes'
-    // request.body.aanmelddatum gedefinieerd als de huidige datum en tijd in een ISO-string-formaat met behulp van toISOString().
-    request.body.aanmelddatum = (new Date()).toISOString();
-    // postJson()-functie opgeroepen, die een HTTP POST-verzoek verzendt naar de opgegeven API-eindpunt en wacht op een respons. De postJson()-functie stuurt de gegevens uit het POST-verzoek op als JSON-geformatteerde gegevens in de request.body.
-    postJson(url, request.body).then((data) => {
-        let newStekje = {...request.body}
 
-        // Als het POST-verzoek succesvol was, wordt de client doorverwezen naar het hoofdmenu van de webapplicatie, zoals aangegeven door response.redirect('/').
-        if (data.success) {
-            response.redirect('/')
-
-            // Als het POST-verzoek niet succesvol was, wordt er een foutbericht gegenereerd met behulp van de inhoud van de respons.
-        } else {
-            const errormessage = `${data.message}: Mogelijk komt dit door de slug die al bestaat.`
-            // De newStekje variabele wordt gedefinieerd om een kopie van de gegevens van het POST-verzoek te bevatten, en deze variabele wordt gebruikt in de response.render()-functie om ervoor te zorgen dat de gebruiker zijn oorspronkelijke invoer kan zien nadat de foutmelding is weergegeven.
-            const newdata = {error: errormessage, values: newStekje}
-            console.log(data)
-            console.log(JSON.stringify(data))
-            response.render('admin', newdata)
-        }
-    })
-})
+// server.post('/new', (request, response) => {
+//     // Het samenvoegen van de juiste API-eindpunten in de url-variabele.
+//     const baseUrl = "https://api.buurtcampus-oost.fdnd.nl/api/v1"
+//     const url = baseUrl + '/stekjes'
+//     // request.body.aanmelddatum gedefinieerd als de huidige datum en tijd in een ISO-string-formaat met behulp van toISOString().
+//     request.body.aanmelddatum = (new Date()).toISOString();
+//     // postJson()-functie opgeroepen, die een HTTP POST-verzoek verzendt naar de opgegeven API-eindpunt en wacht op een respons. De postJson()-functie stuurt de gegevens uit het POST-verzoek op als JSON-geformatteerde gegevens in de request.body.
+//     postJson(url, request.body).then((data) => {
+//         let newStekje = {...request.body}
 
 // Start een http server op het ingestelde poortnummer en log de url
 http.listen(port, () => {
@@ -181,11 +77,11 @@ http.listen(port, () => {
  * @param {*} url the api endpoint to address
  * @returns the json response from the api endpoint
  */
-// async function fetchJson(url) {
-//     return await fetch(url)
-//         .then((response) => response.json())
-//         .catch((error) => error)
-// }
+async function fetchJson(url) {
+    return await fetch(url)
+        .then((response) => response.json())
+        .catch((error) => error)
+}
 
 /**
  * postJson() is a wrapper for the experimental node fetch api. It fetches the url
